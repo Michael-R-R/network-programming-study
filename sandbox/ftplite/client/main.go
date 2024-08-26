87a3737fb73dee4f72be265d882d09f504c9a30e
Michael-R-R <mikerule68@gmail.com>
2024-08-25 20:08:22 -0700

    formatting

--- sandbox/ftplite/client/main.go
+++ sandbox/ftplite/client/main.go
@@ -35,21 +35,7 @@ func main() {
 		switch cmd {
 		case "ls":
 			{
-				conn.Write([]byte(cmd))
-
-				var buf [512]byte
-				n, err := conn.Read(buf[:])
-				if err != nil {
-					fmt.Println(err)
-					continue
-				}
-
-				var dl DirList
-				json.Unmarshal(buf[0:n], &dl)
-
-				for _, d := range dl.Directories {
-					fmt.Println(d)
-				}
+				handleLS(cmd, conn)
 			}
 		case "exit":
 			{
@@ -63,4 +49,24 @@ func main() {
 			}
 		}
 	}
+}
+
+func handleLS(cmd string, conn net.Conn) {
+	conn.Write([]byte(cmd))
+
+	var buf [512]byte
+	n, err := conn.Read(buf[:])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var dl DirList
+	json.Unmarshal(buf[0:n], &dl)
+
+	fmt.Println("---------")
+	for _, d := range dl.Directories {
+		fmt.Println(d)
+	}
+	fmt.Println("---------")
 }
